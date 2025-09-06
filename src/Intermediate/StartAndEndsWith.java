13cb9e324d921230ff3ba18843a876f18ca305d2
PIYUSH GARG <piyushgarglive@gmail.com>
2025-09-06 18:30:51 +0530

    Added new question

---
+++ src/Intermediate/StartAndEndsWith.java
@@ -0,0 +1,18 @@
+package Intermediate;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class StartAndEndsWith {
+
+	public static void main(String[] args) {
+		//Find all words that starts and ends with same character
+		 String sentence="apple anna civic banana level";
+		 
+		 List<String> words = Arrays.stream(sentence.split("\\s+")).filter(x -> x.charAt(0) == x.charAt(x.length()-1)).toList();
+
+		 System.out.println(words);
+		 
+	}
+
+}
